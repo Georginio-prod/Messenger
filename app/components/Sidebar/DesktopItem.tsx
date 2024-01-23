31f217437e21f752670c8865af4e94c716abf4e2
Georginio <etonameklou6@gmail.com>
2024-01-23 14:34:25 +0000

    ajout des composant item

---
+++ app/components/Sidebar/DesktopItem.tsx
@@ -0,0 +1,9 @@
+const DesktopItem = () =>{
+    return(
+        <div>
+            je fd
+        </div>
+    )
+}
+
+export default DesktopItem;
---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,38 @@
+import { useMemo} from "react";
+import { usePathname} from "next/navigation";
+import { HiChat} from "react-icons/hi";
+import { HiOutlineArrowLeftOnRectangle
+    ,HiUsers
+} from "react-icons/hi2"
+import { signOut } from "next-auth/react";
+import useConversation from "@/app/hooks/useConversation";
+
+const useRoutes = () => {
+    const pathname = usePathname();
+    const {conversationId} = useConversation();
+
+    const routes = useMemo(() =>[
+        {
+            label: 'chat',
+            href: '/converstations',
+            icon: HiChat,
+            active: pathname === 'conversations' || !! conversationId
+        },
+        {
+            label: 'Users',
+            href:'/users',
+            icon: HiUsers,
+            active:pathname ==='/users'
+        },
+        {
+            label: 'logout',
+            href: '#',
+            onClick: () => signOut(),
+            icon: HiOutlineArrowLeftOnRectangle
+        }
+], [pathname, conversationId]);
+
+    return routes;
+}
+
+export default useRoutes;
